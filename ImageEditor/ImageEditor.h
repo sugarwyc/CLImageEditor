ed5b91d0bc19b8ad44cc0d28c03023e8d1c57ea9
王义川 <2978439075@qq.com>
2016-04-19 11:32:20 +0800

    增加carthage支持

---
+++ ImageEditor/ImageEditor.h
@@ -0,0 +1,21 @@
+//
+//  ImageEditor.h
+//  ImageEditor
+//
+//  Created by 王义川 on 16/4/19.
+//  Copyright © 2016年 王义川. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageEditor.
+FOUNDATION_EXPORT double ImageEditorVersionNumber;
+
+//! Project version string for ImageEditor.
+FOUNDATION_EXPORT const unsigned char ImageEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageEditor/PublicHeader.h>
+
+#import <ImageEditor/CLImageEditor.h>
+#import <ImageEditor/CLImageToolInfo.h>
+#import <ImageEditor/CLImageEditorTheme.h>
